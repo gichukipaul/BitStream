c9bc4fc28318c1560dd11bf361d4d840c2cf28ad
GichukiPaul <gichukipaul043@gmail.com>
2025-04-05 18:45:59 +0300

    Initial Commit

---
+++ BitStream/BitStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitStreamApp.swift
+//  BitStream
+//
+//  Created by GICHUKI on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitStreamTests/BitStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitStreamTests.swift
+//  BitStreamTests
+//
+//  Created by GICHUKI on 05/04/2025.
+//
+
+import Testing
+@testable import BitStream
+
+struct BitStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
